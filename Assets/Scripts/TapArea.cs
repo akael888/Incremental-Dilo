27b2f3e7456e705b1895470ff47e773f319cea89
Elgratio Latuihamallo <akael888@gmail.com>
2021-10-20 23:48:25 +0700

    Project Main

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+    public int tapCounter = 0;
+    public void OnPointerDown(PointerEventData eventData)
+
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        tapCounter++;
+        AchievementController.Instance.UnlockAchievement(AchievementType.ClickedReached, tapCounter.ToString());
+    }
+
+}
